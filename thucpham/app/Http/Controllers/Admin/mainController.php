9f623e726b22a29ac7ec2730fa63f434dfb47d14
Kuneksa <59677105+tripham208@users.noreply.github.com>
2021-11-11 23:38:40 +0700

    dashboard2
    thêm đồ thị doanh thu năm

--- thucpham/app/Http/Controllers/Admin/mainController.php
+++ thucpham/app/Http/Controllers/Admin/mainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use function MongoDB\BSON\toJSON;
+use function Sodium\add;
 
 class mainController extends Controller
 {
@@ -17,12 +19,22 @@ class mainController extends Controller
         if ($doanhthunam[0]->doanhthu!=null) $nam=$doanhthunam[0]->doanhthu;
         $dondat=\DB::table('donhang')->where("loaidon",3)->count();
         $vandon=\DB::table('donhang')->where("loaidon",4)->count();
+
+
+        $ds=[];
+        for ($x = 1; $x <= 12; $x++){
+            $ds[]=\DB::table('thisyear')->where("thang",$x)->sum("tongtien");
+        }
+
+
+
         return view('admin.dashboard', [
             'title' => 'admin',
             'doanhthuthang'=> $thang,
             'doanhthunam'=> $nam,
             'dondat'=>$dondat,
-            'vandon'=>$vandon
+            'vandon'=>$vandon,
+            'list'=>$ds
         ]);
         /*
          * create view hoadonnam as
@@ -36,6 +48,13 @@ from `thucpham`.`donhang` `hd`
 where year(`hd`.`thoigian`) = year(curtime())
   and month(`hd`.`thoigian`) = month(curtime())
   and `hd`.`loaidon` = 2;
+create view thisyear as
+select month(`hd`.`thoigian`) AS `thang`, `hd`.`tongtien` AS `tongtien`
+from `thucpham`.`donhang` `hd`
+where year(`hd`.`thoigian`) = year(curtime())
+  and `hd`.`loaidon` = 2;
+
+
          */
 
     }
